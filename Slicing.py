5b302961edef634e838d965b2ed7ee6b339a7cc3
salinamary97 <salina.mary97@gmail.com>
2019-09-24 16:47:33 +0530

    basic python sytax examples

---
+++ Slicing.py
@@ -0,0 +1,47 @@
+#s1= "Twinkle,twinkle,little star,"
+#s2= "How I wonder what you are!"
+#s3 = "up above the world so high,"
+#s4 = "Like a diamond in the sky."
+#print("Twinkle,twinkle,little star,\n \t How I wonder what you are!\n \t \t up above the world so high\n \t \t Like a diamond in the sky \n Twinkle,twinkle,little star, \n \t Like a diamond in the sky")
+#print("1",end='')
+#print("2",end='')
+#print("3",end='')
+#print("4",end='')
+#print("5",end='')
+#print("6",end='')
+#print("7",end='')
+#print("8",end='')
+#print("9")
+#a,b = 2,3
+#print("2","3",sep=':')
+s = "HELLO WORLD"
+print(s[0])
+print(s[4])
+print(s[-1])
+print(s[1:])
+print(s[:3])
+print(s[::-1])
+print(s[1:7:2])
+print(s[1 : 4])
+s1 = str(12345678)
+print(s1[3])
+print(s1[3:6])
+print(s[1:7:2])
+s3='123456789'
+print(s3[1:5:3])
+str1="Hello this is Salina"
+x= str1.split( )
+print(x)
+print(str1.split('S'))
+str2="Hello"
+print(str2.upper())
+str3="HELLO"
+print(str3.lower())
+x= len(str1)
+print(x)
+str4='salina mary'
+x='B' not in str4
+print(x)
+
+
+
